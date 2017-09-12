52d850b3e0014ad5120348bbeca4e563f5e5215e
yuhou <xupei0808@163.com>
2017-09-12 14:59:00 +0800

    thrift rpc modify by yuhou

---
+++ example-server/src/main/java/com/laoge/raining/Application.java
@@ -0,0 +1,15 @@
+package com.laoge.raining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yuhou on 2017/9/12.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
