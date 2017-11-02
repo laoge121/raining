6d2dd541a0d7b5d15c1f4d97cbea928cd25c8841
laoge121 <xupei0808@163.com>
2017-11-02 20:15:27 +0800

    升级自有协议 modify yuhou

--- example-client/src/main/java/com/laoge/raining/client/controller/NiceController.java
+++ example-client/src/main/java/com/laoge/raining/client/controller/NiceController.java
@@ -1,9 +1,11 @@
 package com.laoge.raining.client.controller;
 
 import com.laoge.raining.api.entity.Person;
+import com.laoge.raining.api.example.HellowService;
 import com.laoge.raining.api.example.NiceService;
 import com.laoge.raining.client.annotation.RainClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -18,11 +20,27 @@ public class NiceController {
     @RainClient(appname = "raining")
     private NiceService niceService;
 
+    @RainClient
+    private HellowService hellowService;
+
     @RequestMapping("/nice")
+    @ResponseBody
     public String nice() {
         Person person  = new Person();
         person.setName("ketty");
         String str = niceService.sayNice(person);
         return str;
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+      return   hellowService.sayHello();
+    }
+
+    @RequestMapping("/hello1")
+    @ResponseBody
+    public String hello1(){
+        return   hellowService.sayHello(new Person()).toString();
     }
 }
